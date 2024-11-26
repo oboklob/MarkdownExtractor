2852401a0402f09988f7ccb88a60e873c7039ad7
Stuart Gallemore <stuart@nameless.co.uk>
2024-11-26 15:25:40 +0000

    bump up requests version due to vulnerability

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='Markdown Extract',
-    version='0.1.7d',
+    version='0.1.7e',
     url='https://bitbucket.org/nameless-media/markdown-extract',
     author='Stuart Gallemore',
     author_email='stuart@tiscreport.org',

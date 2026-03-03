95dfc33ddbc336d549d331a43a14c6f30b06283a
stuart <stuart@nameless.co.uk>
2026-03-03 12:30:58 +0000

    chore: bump package version to 0.2.2b

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='Markdown Extract',
-    version='0.2.1b',
+    version='0.2.2b',
     url='https://bitbucket.org/nameless-media/markdown-extract',
     author='Stuart Gallemore',
     author_email='stuart@tiscreport.org',
